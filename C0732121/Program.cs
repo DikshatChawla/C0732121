b86d7da4780bc7ddd65795fed8c3b03ab1c3ddaf
visualstudio <visualstudio@122-PC25>
2019-01-30 11:06:02 -0500

    hb

--- C0732121/Program.cs
+++ C0732121/Program.cs
@@ -4,12 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0732121
-{
-    class Program
+
+    namespace C032121
     {
-        static void Main(string[] args)
+        class Program
         {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Hello World!");
+            }
+        }
+
+        class Node
+        {
+            class Village
+
+            {
+                // Node is an ADT
+                // what KInd of DATA do we need in a NODE?
+                public Village nextVillage;
+                public String VillageName;
+                public bool isAstrildeHere = false;
+
+            }
+
         }
     }
-}
+
